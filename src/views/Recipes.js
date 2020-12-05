0595e1feeef34da14d79bcf0bfdaf6b9377c6e47
Dana Pham <danapham12@gmail.com>
2020-12-05 11:10:09 -0600

    routes are set up

---
+++ src/views/Recipes.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Recipes extends Component {
+  state = { }
+
+  render() {
+    return (
+      <h1>Recipes</h1>
+    );
+  }
+}
+
+export default Recipes;
---
+++ src/views/SingleRecipe.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SingleRecipe extends Component {
+  state = { }
+
+  render() {
+    return (
+      <h1>Single Recipe</h1>
+    );
+  }
+}
+
+export default SingleRecipe;
