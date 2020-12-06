54574b21a57b76627adcd47f1e7df21af07e2f1a
Dana Pham <danapham12@gmail.com>
2020-12-06 14:48:10 -0600

    have app modal running

---
+++ src/components/Forms/RecipeForm.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {
+  Button, Form, FormGroup, Label, Input, FormText,
+} from 'reactstrap';
+
+const RecipeForm = (props) => (
+    <Form>
+      <FormGroup>
+        <Label for="recipeName">Recipe Name</Label>
+        <Input type="text" name="name" placeholder="ex. Butternut Squash Soup" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="recipeDescription">Description</Label>
+        <Input type="text" name="description" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="exampleSelect">Select</Label>
+        <Input type="select" name="select" id="exampleSelect">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </Input>
+      </FormGroup>
+      <FormGroup>
+        <Label for="exampleSelectMulti">Select Multiple</Label>
+        <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </Input>
+      </FormGroup>
+      <FormGroup>
+        <Label for="exampleText">Text Area</Label>
+        <Input type="textarea" name="text" id="exampleText" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="exampleFile">File</Label>
+        <Input type="file" name="file" id="exampleFile" />
+        <FormText color="muted">
+          This is some placeholder block-level help text for the above input.
+          It's a bit lighter and easily wraps to a new line.
+        </FormText>
+      </FormGroup>
+      <FormGroup tag="fieldset">
+        <legend>Radio Buttons</legend>
+        <FormGroup check>
+          <Label check>
+            <Input type="radio" name="radio1" />{' '}
+            Option one is this and that—be sure to include why it's great
+          </Label>
+        </FormGroup>
+        <FormGroup check>
+          <Label check>
+            <Input type="radio" name="radio1" />{' '}
+            Option two can be something else and selecting it will deselect option one
+          </Label>
+        </FormGroup>
+        <FormGroup check disabled>
+          <Label check>
+            <Input type="radio" name="radio1" disabled />{' '}
+            Option three is disabled
+          </Label>
+        </FormGroup>
+      </FormGroup>
+      <FormGroup check>
+        <Label check>
+          <Input type="checkbox" />{' '}
+          Check me out
+        </Label>
+      </FormGroup>
+      <Button>Submit</Button>
+    </Form>
+);
+
+export default RecipeForm;
--- src/views/Recipes.js
+++ src/views/Recipes.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import AppModal from '../components/AppModal';
+import RecipeForm from '../components/Forms/RecipeForm';
 
 class Recipes extends Component {
   state = { }
 
   render() {
     return (
+      <>
       <h1>Recipes</h1>
+      <AppModal buttonLabel={<i class="fas fa-plus"></i>} title="Add a Recipe">
+        <RecipeForm />
+      </AppModal>
+      </>
     );
   }
 }
