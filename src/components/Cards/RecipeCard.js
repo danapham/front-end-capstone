0f76e704026df0b57b81d4e7acb35fd4776dcd8d
Dana Pham <danapham12@gmail.com>
2020-12-13 11:23:36 -0600

    recipes & recipe ingredients now get deleted

--- src/components/Cards/RecipeCard.js
+++ src/components/Cards/RecipeCard.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Card, CardText } from 'reactstrap';
+import { Card, CardText, Button } from 'reactstrap';
+import recipeData from '../../helpers/data/recipeData';
 
 class RecipeCard extends Component {
   state = { }
+
+  deleteRecipe = (e) => {
+    e.preventDefault();
+
+    recipeData.deleteRecipe(e.target.id);
+  }
 
   render() {
     const { recipe } = this.props;
@@ -12,6 +19,7 @@ class RecipeCard extends Component {
       <Link className="recipe-card-link" to={`/single-recipe/${recipe.recipeId}`}>
         <Card className="recipe-card">
           <CardText className="recipe-card-text">{recipe.recipeName}</CardText>
+          <Button id={recipe.recipeId} onClick={(e) => this.deleteRecipe(e)}><i class="far fa-trash-alt"></i></Button>
         </Card>
       </Link>
       </>
